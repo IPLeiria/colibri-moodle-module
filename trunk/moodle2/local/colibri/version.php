ebbd6b4e00b36f5b22bb746bb1a8279d70d308bb
cesperanc <cesperanc@5ad665b2-2184-d3ec-53f2-31d6d2f5e493>
2011-04-11 19:53:36 +0000

    SVN property set

--- trunk/moodle2/local/colibri/version.php
+++ trunk/moodle2/local/colibri/version.php
@@ -4,13 +4,16 @@
  *
  * @package    	Colibri
  * @subpackage 	local_colibri
- * @copyright 	Learning Distance Unit {@link http://ued.ipleiria.pt} - Polytechnic Institute of Leiria
+ * @copyright 	{@link http://ued.ipleiria.pt | Learning Distance Unit } - Polytechnic Institute of Leiria
  * @author 	Cláudio Esperança <claudio.esperanca@ipleiria.pt>
- * @license	http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license	{@link http://www.gnu.org/copyleft/gpl.html |  GNU GPL v3 or later}
  *
- * $Author$
- * $Revision$
- * $LastChangedDate$
+ * @author         $Author$
+ * @copyright      $Copyright$
+ * @version        $Rev$
+ * @lastrevision   $Date$
+ * @license        $License$
+ * @filesource     $URL$
  */
 $plugin->version = 2011040800;
 $plugin->requires = 2011033002;
